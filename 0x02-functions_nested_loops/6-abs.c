be3789da66a23c1111faebb2be7a24be724f1cf0
Abelawelachew <abelaw84@gmail.com>
2022-04-13 11:48:59 +0300

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,25 +2,110 @@
 #include <stdio.h>
 
 /**
- * _abs - absolute value
- * 
- * @n: The int to print
- * Return: always 0.
+ * _abs - value absolute
+ *
+ *@n: The int to print
+ * Return: Always 0.
  */
+
 int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (n * (-1));
-	}
 
-	else if (n == 0)
-	{
-		return (0);
-	}
-
-	else
-	{
-		return (n);
-	}
+if (n < 0)
+{
+return (n * (-1));
 }
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+6-abs.c[+] [unix] (11:33 13/04/2022)                            1,1 All
+-- INSERT --
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+6-abs.c[+] [unix] (11:33 13/04/2022)                            1,1 All
+-- INSERT --
+
