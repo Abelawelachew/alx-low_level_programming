4b0ddfa2f32b7d9bb17c4e3e0d6b67ebb4173977
Abelawelachew <abelaw84@gmail.com>
2022-04-13 11:46:30 +0300

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,15 +8,15 @@
 int _islower(int c)
 
 {
-	if (c > 'a' && c < 'z')
-	{
-		return (1);
-	}
 
-	else
-	{
-		return (0);
-	}
-	
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
 
 }
